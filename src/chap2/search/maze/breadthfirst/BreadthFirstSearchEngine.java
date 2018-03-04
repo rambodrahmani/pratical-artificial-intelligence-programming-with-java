25a95e77e88032cca73705b983aeafc0c74910db
Rambod Rahmani <rambodrahmani@yahoo.it>
2018-03-05 00:39:01 +0100

    Added new comments.

--- src/chap2/search/maze/breadthfirst/BreadthFirstSearchEngine.java
+++ src/chap2/search/maze/breadthfirst/BreadthFirstSearchEngine.java
@@ -9,12 +9,18 @@ import chap2.search.maze.AbstractSearchEngine;
 import chap2.search.maze.Location;
 
 /**
+ * The derived class BreadthF irstSearch is similar to the DepthF irstSearch
+ * procedure with one major difference: from a specified search location we
+ * calculate all possible moves, and make one possible trial move at a time.
+ * 
  * @author Rambod Rahmani &lt;rambodrahmani@autistici.org&gt;
  */
 public class BreadthFirstSearchEngine extends AbstractSearchEngine
 {
     /**
-     * Default constructor.
+     * Default constructor. The class constructor for BreadthF irstSearch calls the
+     * super class constructor to initialize the maze, and then uses the auxiliary
+     * method doSearchOn2DGrid for performing a breadth first search for the goal.
      * 
      * @param rows
      * @param columns
@@ -27,14 +33,19 @@ public class BreadthFirstSearchEngine extends AbstractSearchEngine
     }
 
     /**
-     * 
+     * The method doSearchOn2Dgrid is not recursive, it uses a loop to add new
+     * search positions to the end of an instance of class DimensionQueue and to
+     * remove and test new locations from the front of the queue. The
+     * two-dimensional array alreadyVisitedFlag keeps us from searching the same
+     * location twice. To calculate the shortest path after the goal is found, we
+     * use the predecessor array:
      */
     private void doSearchOn2DGrid()
     {
 	int rows = getMaze().getRows();
 	int columns = getMaze().getColumns();
 
-	boolean alReadyVisitedFlag[][] = new boolean[rows][columns];
+	boolean alreadyVisitedFlag[][] = new boolean[rows][columns];
 
 	// float distanceToNode[][] = new float[width][height];
 	Location predecessor[][] = new Location[rows][columns];
@@ -44,13 +55,13 @@ public class BreadthFirstSearchEngine extends AbstractSearchEngine
 	{
 	    for (int j = 0; j < columns; j++)
 	    {
-		alReadyVisitedFlag[i][j] = false;
+		alreadyVisitedFlag[i][j] = false;
 		// distanceToNode[i][j] = 10000000.0f;
 		predecessor[i][j] = null;
 	    }
 	}
 
-	alReadyVisitedFlag[getStartLocation().getColumn()][getStartLocation().getRow()] = true;
+	alreadyVisitedFlag[getStartLocation().getColumn()][getStartLocation().getRow()] = true;
 
 	// distanceToNode[startLoc.width][startLoc.height] = 0.0f;
 	queue.addToBackOfQueue(getStartLocation());
@@ -76,10 +87,10 @@ public class BreadthFirstSearchEngine extends AbstractSearchEngine
 
 		int row = connected[i].getRow();
 		int column = connected[i].getColumn();
-		if (alReadyVisitedFlag[row][column] == false)
+		if (alreadyVisitedFlag[row][column] == false)
 		{
 		    // distanceToNode[w][h] = distanceToNode[w][h] + 1.0f;
-		    alReadyVisitedFlag[row][column] = true;
+		    alreadyVisitedFlag[row][column] = true;
 		    predecessor[row][column] = head;
 		    queue.addToBackOfQueue(connected[i]);
 		    if (equals(connected[i], getGoalLocation()))
@@ -115,15 +126,23 @@ public class BreadthFirstSearchEngine extends AbstractSearchEngine
 	}
     }
 
+    /**
+     * We use a queue data structure for storing possible moves, placing possible
+     * moves on the back of the queue as they are calculated, and pulling test moves
+     * from the front of the queue. The effect of a breadth first search is that it
+     * "fans out" uniformly from the starting node until the goal node is found.
+     * 
+     * @author Rambod Rahmani &lt;rambodrahmani@autistici.org&gt;
+     */
     protected class LocationQueue
     {
 	private Location[] queue = null;
 
-	private int	   tail	 = -1;
+	private int tail = -1;
 
-	private int	   head	 = -1;
+	private int head = -1;
 
-	private int	   len	 = -1;
+	private int len = -1;
 
 	public LocationQueue(int num)
 	{
